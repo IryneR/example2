37b0a448ceaac9fb3b1428c0d13f89c1093c1c8c
Iryna Romaniuk <romaniuk.iv@gmail.com>
2020-12-11 14:35:45 +0200

    first commit

---
+++ example2/src/main/java/example2/AddonApplication.java
@@ -0,0 +1,16 @@
+package example2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddonApplication {
+
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(AddonApplication.class).run(args);
+    }
+}
+
