74aeb9ec5b499f1ee62726f62e1586eee81c99e6
Iryna Romaniuk <romaniuk.iv@gmail.com>
2020-12-15 13:03:17 +0200

    get issue fields which are going to be stored in db

--- example2/src/main/java/example2/ThymeleafController.java
+++ example2/src/main/java/example2/ThymeleafController.java
@@ -1,14 +1,47 @@
 package example2;
 
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import example2.dto.IssueDto;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Controller
 public class ThymeleafController {
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    private ObjectMapper mapper = new ObjectMapper();
 
     @GetMapping("/thymeleaf")
     public String getIframe() {
         return "thymeleaf";
     }
+
+    @RequestMapping(value = "/webhook-received", method = RequestMethod.POST)
+    @ResponseBody
+    public String onWebhookReceived(@RequestBody Map<String, Object> request) throws JsonProcessingException {
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        System.out.println(String.format("Received webhook (%s) from Jira for webhook ids: %s\n\n%s",
+                request.get("webhookEvent"),
+                request.get("matchedWebhookIds"),
+                request.containsKey("issue") ? GSON.toJson(request.get("issue")) : ""));
+
+        IssueDto issueDto = mapper.readValue(GSON.toJson(request.get("issue")), IssueDto.class);
+        System.out.println(issueDto);
+        return "{\"result\": \"ok\"}";
+    }
+
+/*
+    private LinkedMultiValueMap<String, String> baseHeaders() {
+        LinkedMultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add("Content-Type", "application/json");
+        headers.add("Accept", "application/json");
+        return headers;
+    }*/
 }
---
+++ example2/src/main/java/example2/dto/IssueDto.java
@@ -0,0 +1,49 @@
+package example2.dto;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.ToString;
+
+import java.util.Map;
+
+@ToString
+@JsonAutoDetect
+public class IssueDto {
+    private String issueKey;
+    private String issueTypeName;
+    private String projectKey;
+    private String creatorDisplayName;
+
+    public String getIssueKey() {
+        return issueKey;
+    }
+
+    @JsonProperty("key")
+    public void setIssueKey(String key) {
+        this.issueKey = key;
+    }
+
+    public String getIssueTypeName() {
+        return issueTypeName;
+    }
+
+    @JsonProperty("fields")
+    public void setIssueFields(Map<String, Object> fields) {
+        Map<String, Object> issueType = (Map<String, Object>) fields.get("issuetype");
+        this.issueTypeName = (String) issueType.get("name");
+        Map<String, Object> project = (Map<String, Object>) fields.get("project");
+        this.projectKey = (String) project.get("name");
+        Map<String, Object> creator = (Map<String, Object>) fields.get("creator");
+        this.creatorDisplayName = (String) creator.get("displayName");
+    }
+
+    public String getProjectKey() {
+        return projectKey;
+    }
+
+    public String getCreatorDisplayName() {
+        return creatorDisplayName;
+    }
+
+}
+
