0de1a24f5fc692c96abb5b4e78c44b76e34f79b5
Irina Romanyuk <romaniuk.iv@gmail.com>
2021-01-04 17:19:19 +0200

    make changes in order to add python service to docker

--- src/main/java/example2/services/IssueService.java
+++ src/main/java/example2/services/IssueService.java
@@ -5,6 +5,7 @@ import example2.dto.IssueDto;
 import example2.model.JiraHash;
 import example2.repositories.IssueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,9 @@ import java.util.*;
 public class IssueService {
     @Autowired
     private IssueRepository issueRepository;
+
+    @Value("${application.hash-service-url}")
+    private String hashServiceUrl;
 
     public int createIssue(IssueDto issueDto) {
         IssueEntity issueEntity = new IssueEntity();
@@ -34,7 +38,7 @@ public class IssueService {
     public String getHash() {
         RestTemplate restTemplate = new RestTemplate();
         String hashUrl
-                = "http://127.0.0.1:5000/hash";
+                = hashServiceUrl;
         ResponseEntity<JiraHash> response
                 = restTemplate.getForEntity(hashUrl, JiraHash.class);
         return response.getBody().getHash();
